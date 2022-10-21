f469e6bc23eeb1167259b1dc1abe56645ca0bba8
Alexey Pavlov <pavlov.aleksey.m@gmail.com>
2022-10-21 19:16:39 +0700

    feat: split getNews reducer into two reducers for popular & latest

--- src/redux/slices/newsSlice.js
+++ src/redux/slices/newsSlice.js
@@ -2,27 +2,27 @@ import {createSlice} from "@reduxjs/toolkit";
 
 export const newsSlice = createSlice({
     name: 'news', initialState: {
-        latestNews: [],
-        popularNews: [],
-        latestNewsError: '',
-        popularNewsError: '',
+        latestNews: [], popularNews: [], latestNewsError: '', popularNewsError: '',
     }, reducers: {
-        getNews: (state) => state,
-        setLatestNews: (state, {payload}) => {
-            state.latestNews = [...state.latestNews, ...payload];
-        },
-        setPopularNews: (state, {payload}) => {
-            state.popularNews = [...state.popularNews, ...payload];
-        },
-        setLatestNewsError: (state, {payload}) => {
+        getLatestNews: (state) => state, getPopularNews: (state) => state, setLatestNews: (state, {payload}) => {
+            state.latestNews = payload;
+        }, setPopularNews: (state, {payload}) => {
+            state.popularNews = payload;
+        }, setLatestNewsError: (state, {payload}) => {
             state.latestNewsError = payload
-        },
-        setPopularNewsError: (state, {payload}) => {
+        }, setPopularNewsError: (state, {payload}) => {
             state.popularNewsError = payload
         }
     }
 })
 
-export const {getNews, setLatestNews, setPopularNews, setLatestNewsError, setPopularNewsError} = newsSlice.actions
+export const {
+    getLatestNews,
+    getPopularNews,
+    setLatestNews,
+    setPopularNews,
+    setLatestNewsError,
+    setPopularNewsError
+} = newsSlice.actions
 
 export default newsSlice.reducer
